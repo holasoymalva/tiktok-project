6b33423eda44226e9c4f7597168bca3bccf07ba2
Martin <martinmanriquez@iMac-de-Martin.local>
2023-02-06 22:51:01 -0600

    [ft]:first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const { WebcastPushConnection } = require('tiktok-live-connector');
+let tiktokUsername = "holasoymalva";
+let tiktokLiveConnection = new WebcastPushConnection(tiktokUsername);
+
+tiktokLiveConnection.connect().then(state => {
+
+}).catch(err => {
+    console.error('Failed to connect', err);
+})
+
+tiktokLiveConnection.on('gift', data => {
+    if (data.giftType === 1 && !data.repeatEnd) {
+        console.log(`${data.nickname} muchas gracias!! 🦸`);
+    } else {
+        console.log(`${data.nickname} muchas gracias por tu regalo!! 🦸🎁 `);
+    }
+})
+
+tiktokLiveConnection.on('like', data => {
+    console.log(`${data.nickname} muchas gracias por tus likes 👩‍🚀😸`);
+})
+
+tiktokLiveConnection.on('follow', (data) => {
+    console.log(data.nickname, "gracias por tu follow!!! 🦄✨");
+})
+
